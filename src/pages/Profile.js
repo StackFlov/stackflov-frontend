3523bd06eaa197bafafb7b71bd9f56c9099f3a52
WePung <akgre5@naver.com>
2025-10-19 19:37:56 +0900

    Edit : Profile
    - 어드민 페이지 진입 메뉴 생성

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserInfos from "../components/Profiles/UserInfos";
 import {
   ProfileContentDiv,
@@ -10,10 +10,38 @@ import {
 import UserPosts from "../components/Profiles/UserPosts";
 import UserReplys from "../components/Profiles/UserReplys";
 import UserBookMarks from "../components/Profiles/UserBookMarks";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const [me, setMe] = useState();
+  const accessToken = Cookies.get("accessToken");
   const [selectMenu, setSelectMenu] = useState(0);
+  const navigator = useNavigate();
   const Menus = { 0: "내 프로필", 1: "내 게시글", 2: "내 댓글", 3: "북마크" };
+
+  useEffect(() => {
+    const token = Cookies.get("accessToken");
+
+    if (token) {
+      axios
+        .get("https://api.stackflov.com/users/me", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
+        })
+        .then((res) => {
+          setMe(res.data);
+        })
+        .catch((err) => {
+          console.error("Error fetching user data:", err);
+        });
+    }
+  }, [accessToken]);
+
   return (
     <ProfileWrapper>
       <ProfileTopDiv>마이 페이지</ProfileTopDiv>
@@ -50,6 +78,16 @@ const Profile = () => {
         >
           북마크
         </ProfileMenuItem>
+        {me?.role == "ADMIN" && (
+          <ProfileMenuItem
+            select={selectMenu == 3 ? true : false}
+            onClick={() => {
+              navigator("/admin");
+            }}
+          >
+            어드민
+          </ProfileMenuItem>
+        )}
       </ProfileSideDiv>
       <ProfileContentDiv>
         {selectMenu == 0 && <UserInfos />}
