9935b40ba906172844b5f0ddbff4abef2212ce6d
WePung <akgre5@naver.com>
2025-10-25 15:19:18 +0900

    Edit : searchBar
    - 리뷰에는 카테고리가 없어 카테고리 관련 내용 니방내방일 때는 미적용

--- src/components/NiBangNeBang/NiBangNeBangSelector.js
+++ src/components/NiBangNeBang/NiBangNeBangSelector.js
@@ -6,53 +6,58 @@ import {
   NiBangNeBangSelectorWrapper,
   TraceCreateDiv,
 } from "../../styles/components/NiBangNeBangSelectorStyled";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const NiBangNeBangSelector = ({ nowCategory, setNowCategory }) => {
+  const location = useLocation();
   const navigator = useNavigate();
   return (
     <NiBangNeBangSelectorWrapper>
       <NiBangNeBangSelectors>
-        <NiBangNeBangSelectorItem
-          selectCategory={nowCategory == 99 ? true : false}
-          onClick={() => {
-            setNowCategory(99);
-          }}
-        >
-          전체
-        </NiBangNeBangSelectorItem>
-        <NiBangNeBangSelectorItem
-          selectCategory={nowCategory == 1 ? true : false}
-          onClick={() => {
-            setNowCategory(1);
-          }}
-        >
-          🏠 자취
-        </NiBangNeBangSelectorItem>
-        <NiBangNeBangSelectorItem
-          selectCategory={nowCategory == 2 ? true : false}
-          onClick={() => {
-            setNowCategory(2);
-          }}
-        >
-          ⚡ 번개
-        </NiBangNeBangSelectorItem>
-        <NiBangNeBangSelectorItem
-          selectCategory={nowCategory == 3 ? true : false}
-          onClick={() => {
-            setNowCategory(3);
-          }}
-        >
-          🍯️ 꿀팁
-        </NiBangNeBangSelectorItem>
-        <NiBangNeBangSelectorItem
-          selectCategory={nowCategory == 4 ? true : false}
-          onClick={() => {
-            setNowCategory(4);
-          }}
-        >
-          🍙 레시피
-        </NiBangNeBangSelectorItem>
+        {location.pathname == "/" && (
+          <>
+            <NiBangNeBangSelectorItem
+              selectCategory={nowCategory == 99 ? true : false}
+              onClick={() => {
+                setNowCategory(99);
+              }}
+            >
+              전체
+            </NiBangNeBangSelectorItem>
+            <NiBangNeBangSelectorItem
+              selectCategory={nowCategory == 1 ? true : false}
+              onClick={() => {
+                setNowCategory(1);
+              }}
+            >
+              🏠 자취
+            </NiBangNeBangSelectorItem>
+            <NiBangNeBangSelectorItem
+              selectCategory={nowCategory == 2 ? true : false}
+              onClick={() => {
+                setNowCategory(2);
+              }}
+            >
+              ⚡ 번개
+            </NiBangNeBangSelectorItem>
+            <NiBangNeBangSelectorItem
+              selectCategory={nowCategory == 3 ? true : false}
+              onClick={() => {
+                setNowCategory(3);
+              }}
+            >
+              🍯️ 꿀팁
+            </NiBangNeBangSelectorItem>
+            <NiBangNeBangSelectorItem
+              selectCategory={nowCategory == 4 ? true : false}
+              onClick={() => {
+                setNowCategory(4);
+              }}
+            >
+              🍙 레시피
+            </NiBangNeBangSelectorItem>
+          </>
+        )}
         <TraceCreateDiv
           onClick={() => {
             navigator("/nibangnebang/create");
